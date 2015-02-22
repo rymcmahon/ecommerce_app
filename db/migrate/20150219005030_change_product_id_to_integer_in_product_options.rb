dd9474e972e6a94595e52349515204cfa70c65cb
rymcmahon <toedragdangle9@gmail.com>
2015-02-22 16:35:11 -0600

    migration conversion for postgresql

--- db/migrate/20150219005030_change_product_id_to_integer_in_product_options.rb
+++ db/migrate/20150219005030_change_product_id_to_integer_in_product_options.rb
@@ -1,7 +1,7 @@
 class ChangeProductIdToIntegerInProductOptions < ActiveRecord::Migration
   def change
   	change_table :product_options do |t|
-  		t.change :product_id, :integer, 'integer USING CAST(product_id AS integer)'
+  		t.change :product_id, 'integer USING CAST(product_id AS integer)'
   	end
   end
 end
